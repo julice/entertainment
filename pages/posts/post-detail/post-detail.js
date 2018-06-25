8ddc82d393f8e06225af747a856a6c949c3733e6
julice <736419409@qq.com>
2018-06-25 17:43:58 +0800

    add music

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -5,7 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    postdetail:''
+    postdetail:'',
+    isplaying:false,
+    musicsrc:'/images/music/music-start.png'
   },
 
   /**
@@ -13,28 +15,134 @@ Page({
    */
   onLoad: function (options) {
     var postid = options.id
+    this.data.postid = postid
     var postsdata = postsdataAll.postcontent[postid]
     // this.data.postdetail = postsdata
     this.setData({
       postdetail: postsdata
     })
+    // var postcollected = {
+    //   1:"false",
+    //   2:"true",
+    //   3:"false"
+    // },
+    // wx.clearStorageSync('posts_collected')
+    var postscollected = wx.getStorageSync('posts_collected')
+    if(postscollected){
+      var postcollect = postscollected[postid]
+      this.setData({
+        collected: postcollect
+      })
+    }
+    else{
+      var postscollected = {}
+      postscollected[postid]=false
+      wx.setStorageSync('posts_collected', postscollected)
+    }
+  
+
+  },
+  // collectiontap:function(event){
+  //   var postscollected = wx.getStorageSync('posts_collected');
+  //   var postcollected = postscollected[this.data.postid]
+  //   console.log('postcollected' + postcollected)
+  //   postcollected = !postcollected
+  //   postscollected[this.data.postid] = postcollected
+  //   this.showToast(postscollected, postcollected)
+  //   // this.showModal(postscollected, postcollected)
     
-    wx.setStorage({
-      key: 'key',
-      data: {
-        game:'风暴',
-        list:'dd'
+    
+  // },
+  collectiontap:function(){
+    var that = this
+    wx.getStorage({
+      key: 'posts_collected',
+      success: function(res) {
+        var postscollected = wx.getStorageSync('posts_collected');
+        var postcollected = postscollected[that.data.postid]
+        postcollected = !postcollected
+        postscollected[that.data.postid] = postcollected
+        that.showToast(postscollected, postcollected)
+        // this.showModal(postscollected, postcollected)
       },
-    }),
-    wx.setStorageSync('key2', {maxi:'maxi'})
-    wx.setStorageSync('key1', {dd:'ddd'})
+    })
   },
-  collectiontap:function(event){
-    var key = wx.getStorageSync('key');
-    // console.log(key)
+  showToast: function (postscollected, postcollected){
+    wx.setStorageSync('posts_collected', postscollected)
+    this.setData({
+      collected: postcollected
+    })
+    wx.showToast({
+      title: postcollected?'收藏成功':'取消收藏',
+      duration:1000,
+      icon:'success'
+    })
   },
+  showModal: function (postscollected, postcollected){
+    var that = this;
+    wx.showModal({
+      title: postcollected ? '收藏' : '取消收藏',
+      content: postcollected ? '收藏该文章？' : '取消收藏该文章？',
+      cancelText: '取消',
+      confirmText: '确定',
+      cancelColor: '#f00',
+      confirmColor: '#405f80',
+      success:function(res){
+        
+        if(res.confirm){
+          console.log("res")
+          wx.setStorageSync('posts_collected', postscollected)
+          that.setData({
+            collected: postcollected
+          })
+        }
+      }
+    })
+  },
+  showAction:function(){
+    var itemlist = ['分享给微信好友', '分享给qq好友', '分享到朋友圈']
+    wx.showActionSheet({
+      itemList: itemlist,
+      itemColor: '#405f80',
+      success: function (res) {
+        console.log(res.tapIndex)
+        wx.showModal({
+          title: '用户分享',
+          content: '用户分享到' + itemlist[res.tapIndex],
+        })
+       },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  onmusictap:function(){
+    var postsdata = postsdataAll.postcontent[this.data.postid]
+    var isplay = this.data.isplaying;
+    console.log(postsdata)
+    if(isplay){
+      wx.pauseBackgroundAudio()
+      this.setData({
+        isplaying:false
+      })
+    }
+    else {
+      this.setData({
+        isplaying: true
+      })
+      wx.playBackgroundAudio({
+        dataUrl: postsdata.music.url,
+        title: postsdata.music.title,
+        coverImgUrl: postsdata.music.coverImg,
+        success: function () {
+          console.log("ok")
+        }
+      })
+    }
+  },
+
+
   sharetap:function(){
-    wx.clearStorageSync()
+    this.showAction()
   },
 
   /**
